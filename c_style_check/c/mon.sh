3353496a47e7332d74a42c1ba5355b4d650f02b5
Hoang Tran <hoang.tran@ca.ibm.com>
2022-01-05 19:21:56 -0500

    Initial import

---
+++ c_style_check/c/mon.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+nodemon -e c --exec "make build S=c_style_check && make run S=c_style_check" --watch "$CWD"
---
+++ c_style_check/go/c_style_check.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+const MAX_LINE_LENGTH = 80
+
+var tabs = regexp.MustCompile(`\t`)
+var comma_space = regexp.MustCompile(`,[^ ]`)
+
+/* This one is really tough to get right, so we settle for catching the
+most common mistakes.  Add other operators as necessary and/or feasible. */
+var operator_space      = regexp.MustCompile(`(\w(\+|\-|\*|\<|\>|\=)\w)|(\w(\=\=|\<\=|\>\=)\w)`)
+var comment_line        = regexp.MustCompile(`^\s*\/\*.*\*\/\s*$`)
+var open_comment_space = regexp.MustCompile(`\/\*[^ *\n]`)
+var close_comment_space = regexp.MustCompile(`[^ *]\*\/`)
+var paren_curly_space = regexp.MustCompile(`\)\{`)
+var c_plus_plus_comment = regexp.MustCompile(`\/\/`)
+var semi_space = regexp.MustCompile(`;[^ \s]`)
+
+func checkLine(filename string, line string, n int) {
+	// Strip the terminal newline
+	strings.TrimRight(line, "\n")
+
+	if tabs.MatchString(line) {
+	        fmt.Printf("File: %s, line %d: [TABS]:\n%s", filename, n, line)
+	}
+
+	if len(line) > MAX_LINE_LENGTH {
+		fmt.Printf("File: %s, line %d: [TOO LONG (%d CHARS)]:\n%s\n", filename, n, len(line), line)
+	}
+
+	if open_comment_space.MatchString(line) {
+		fmt.Printf("File: %s, line: %d: [PUT SPACE AFTER OPEN COMMENT]:\n%s\n", filename, n, line)
+	}
+
+        if close_comment_space.MatchString(line) {
+		fmt.Printf("File: %s, line: %d: [PUT SPACE AFTER CLOSE COMMENT]:\n%s\n", filename, n, line)
+	}
+
+	if paren_curly_space.MatchString(line){
+		fmt.Printf("File: %s, line: %d: [PUT SPACE BETWEEN ) AND {]:\n%s\n", filename, n, line)
+	}
+
+	if c_plus_plus_comment.MatchString(line) {
+	        fmt.Printf("File: %s, line: %d: [DON'T USE C++ COMMENTS]:\n%s\n", filename, n, line)
+	}
+
+        if semi_space.MatchString(line) {
+	        fmt.Printf("File: %s, line: %d: [PUT SPACE/NEWLINE AFTER SEMICOLON]:\n%s\n", filename, n, line)
+	}
+
+        if operator_space.MatchString(line) {
+                if !comment_line.MatchString(line) {
+	                fmt.Printf("File: %s, line: %d: [PUT SPACE AROUND OPERATORS]:\n%s\n", filename, n, line)
+	        }
+        }
+
+        if comma_space.MatchString(line) {
+	        fmt.Printf("File: %s, line: %d: [PUT SPACE AFTER COMMA]:\n%s\n", filename, n, line)
+	}
+}
+
+func checkFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	i := 0
+	// read file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		i++ // Start on line 1.
+		line := scanner.Text()
+		checkLine(filename, line, i)
+	}
+}
+
+func main() {
+	usage := `usage: c_style_check filename1 [filename2 ...]`
+
+	if len(os.Args) < 2 {
+		fmt.Println(usage)
+		os.Exit(0)
+	}
+
+	for _, filename := range os.Args[1:] {
+		checkFile(filename)
+	}
+}
