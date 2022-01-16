a0f92903df9f916440e50ec1c512b15eff3e5ffd
Hoang Tran <hoang.tran@ca.ibm.com>
2022-01-16 10:50:23 -0500

    Use constant instead of global var

--- c_style_check/ruby/c_style_check.rb
+++ c_style_check/ruby/c_style_check.rb
@@ -1,56 +1,55 @@
 #!/usr/bin/env ruby
 
-# rubocop:disable Style/GlobalVars
-$MAX_LINE_LENGTH = 80
+MAX_LINE_LENGTH = 80
 
-$Tabs = Regexp.compile('\t+')
-$CommaSpace = Regexp.compile(',[^\s]')
-$OperatorSpace = Regexp.compile('(\w(\+|\-|\*|\<|\>|\=)\w)|(\w(\=\=|\<\=|\>\=)\w)')
-$CommentLine = Regexp.compile('^\s*\/\*.*\*\/\s*$')
-$OpenCommentSpace = Regexp.compile('\/\*[^ *\n]')
-$CloseCommentSpace = Regexp.compile('[^ *]\*\/')
-$ParenCurlySpace = Regexp.compile('\)\{')
-$CppComment = Regexp.compile('\/\/')
-$SemiSpace = Regexp.compile(';[^ \s]')
+Tabs = Regexp.compile('\t+')
+CommaSpace = Regexp.compile(',[^\s]')
+OperatorSpace = Regexp.compile('(\w(\+|\-|\*|\<|\>|\=)\w)|(\w(\=\=|\<\=|\>\=)\w)')
+CommentLine = Regexp.compile('^\s*\/\*.*\*\/\s*$')
+OpenCommentSpace = Regexp.compile('\/\*[^ *\n]')
+CloseCommentSpace = Regexp.compile('[^ *]\*\/')
+ParenCurlySpace = Regexp.compile('\)\{')
+CppComment = Regexp.compile('\/\/')
+SemiSpace = Regexp.compile(';[^ \s]')
 
 def check_line(filename, line, n)
   # Strip the terminal newline.
   line = line.chomp
 
-  if line =~ $Tabs
+  if line =~ Tabs
     puts "File: #{filename}, line #{n}: [TABS]:\n#{line}"
   end
 
-  if line.length > $MAX_LINE_LENGTH
+  if line.length > MAX_LINE_LENGTH
     puts "File: #{filename}, line #{n}: [TOO LONG (#{line.length} CHARS)]:\n#{line}"
   end
 
-  if line =~ $CppComment
+  if line =~ CppComment
     puts "File: #{filename}, line #{n}: [DON'T USE C++ COMMENTS]:\n#{line}"
   end
 
-  if line =~ $OpenCommentSpace
+  if line =~ OpenCommentSpace
     puts "File: #{filename}, line #{n}: [PUT SPACE AFTER OPEN COMMENT]:\n#{line}"
   end
 
-  if line =~ $CloseCommentSpace
+  if line =~ CloseCommentSpace
     puts "File: #{filename}, line #{n}: [PUT SPACE AFTER CLOSE COMMENT]:\n#{line}"
   end
 
-  if line =~ $ParenCurlySpace
+  if line =~ ParenCurlySpace
     puts "File: #{filename}, line #{n}: [PUT SPACE BETWEEN ) AND {]:\n#{line}"
   end
 
-  if line =~ $CommaSpace
+  if line =~ CommaSpace
     puts "File: #{filename}, line #{n}: [PUT SPACE AFTER COMMA]:\n#{line}"
   end
 
-  if line =~ $SemiSpace
+  if line =~ SemiSpace
     puts "File: #{filename}, line #{n}: [PUT SPACE/NEWLINE AFTER SEMICOLON]:\n#{line}"
   end
 
-  if line =~ $OperatorSpace
-    if line !~ $CommentLine
+  if line =~ OperatorSpace
+    if line !~ CommentLine
       puts "File: #{filename}, line #{n}: [PUT SPACE AROUND OPERATORS]:\n#{line}"
     end
   end
